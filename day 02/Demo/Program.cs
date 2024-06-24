fb8f54f9602d0fcab0de6133aee22aba7267d427
abdelrhmanwael <abdowael939@gmail.com>
2024-06-24 07:33:53 +0300

    Add project files.

---
+++ day 02/Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Demo
+{
+    class Program
+    {
+        //Entry Point (Start Point)
+        static void Main()
+        {
+            int X;
+
+            // CLR Will Allocate 4 UnIntialized Bytes At Stack
+            // int : C# KeyWord
+
+            X = 5;
+
+            Int32 Y = 10;
+
+            // CLR Will Allocate 4 UnIntialized Bytes At Stack = 10
+            // Int32 :BCL
+
+            Y = X; //5
+
+            X++;  //6 
+
+            Console.WriteLine(Y);
+        }
+    }
+}
