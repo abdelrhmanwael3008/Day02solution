6b240a7d18b9fd7492cdce57b0a41a38b746dd94
abdelrhmanwael <abdowael939@gmail.com>
2024-06-24 08:04:22 +0300

    ValueType and Refrence Type

---
+++ day 02/Demo/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+     class Point
+    {
+        public int x; //4 bytes
+        public int y; //4 bytes
+    }
+}
--- day 02/Demo/Program.cs
+++ day 02/Demo/Program.cs
@@ -5,23 +5,57 @@
         //Entry Point (Start Point)
         static void Main()
         {
-            int X;
+            #region ValueType
+            //int X;
 
-            // CLR Will Allocate 4 UnIntialized Bytes At Stack
+            //CLR Will Allocate 4 UnIntialized Bytes At Stack
             // int : C# KeyWord
 
-            X = 5;
+            //X = 5;
 
-            Int32 Y = 10;
+            //Int32 Y = 10;
 
-            // CLR Will Allocate 4 UnIntialized Bytes At Stack = 10
-            // Int32 :BCL
+            //CLR Will Allocate 4 UnIntialized Bytes At Stack = 10
+            // Int32: BCL
 
-            Y = X; //5
+            //Y = X; //5
 
-            X++;  //6 
+            //X++;  //6 
 
-            Console.WriteLine(Y);
+            //Console.WriteLine(Y);
+            #endregion
+
+            #region Reference Type
+
+            //Point p1 ;
+
+            ///// Declare For Reference Of Type "Point" , Refering to NULL
+            ///// This Reference 'p1' Can Refer to an Object From type "point" OR another type "Inherting
+            ///// CLR Will Allocate 4 Bytes For the Reference AT STACK
+            ///// CLR WILL Allocate 0 Bytes AT HEAP
+
+            //p1 = new Point(); //has 2 References [p1,p2]( اسمين دلع );
+
+            /////1.Allocate Required Number Of Bytes In Heab (Object size + CLR Overhead Variables)
+            ///// 2.Initialize (Cross out ) Allocted Bytes With Default Value Of Tts Datatype
+            ///// 3.Call user-Defined Constructor if exists
+            ///// 4.Assingn The Reference to a allocated Object
+
+            //Console.WriteLine(p1.x); //0 
+            //Console.WriteLine(p1.y); //0
+
+            //Point p2 = new Point(); //Unreachable Object
+
+            //p2=p1 ;
+
+            //p2.x = 10;
+
+            //Console.WriteLine(p1.x);
+
+
+            #endregion
+
+
         }
     }
 }
